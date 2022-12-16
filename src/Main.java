fb5878bbd20addaa5dc64329e92881f1f6433846
saltanat <saltanatnematillakyzy6@gmail.com>
2022-12-16 16:07:03 +0600

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<Integer> integers = new ArrayList<>(Arrays.asList(4, 6, 7, 8, 9, 19, 2, 12, 13));
+        integers.stream().filter(list -> list % 2 == 0).map(l -> l * l)
+                .toList().stream().max(Integer::compareTo).ifPresent(System.out::println);
     }
 }
